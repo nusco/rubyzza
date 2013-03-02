f4bd9f24fe85369e5c44a2f4ea3ad7b3ee6fb59a
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2013-03-02 17:54:35 +0100

    remove old code
    from the early project I made to learn Ruby

--- old/lib/all_tests.rb
+++
@@ -1,6 +0,0 @@
-require 'test/unit'
-
-Dir['*_test.rb'].each do |test_file_name|
-  print "Adding ", test_file_name, "\n"
-  require test_file_name.chomp(".rb")
-end
--- old/lib/z_error.rb
+++
@@ -1,6 +0,0 @@
-class ZError < Exception
-end
-
-def error(*msg)
-  raise ZError, msg
-end
